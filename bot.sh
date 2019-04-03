3cb63535c5817ab66e119c0bc42e67d0293c240c
jul10l1r4 <jul10l1r4@disroot.org>
2019-04-03 15:42:49 -0300

    xBot criado, funcionalidades criadas, help manual criado

---
+++ bot.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+_send(){
+  curl -X POST -H 'Content-type: application/json' \
+  --data "{\"text\":\"$1\"}" \
+  $YouBot
+}
+
+_warning(){
+  _send ":eyes: *Atenção*: $1"
+}
+
+_error(){
+  _send ":warning: *Erro*: $1"
+}
+
+_sec(){
+  _send ":lock: *Sec.*: $1"
+}
+
+_msg(){
+  _send ":hangloose: *Mensagem*: $1"
+}
+case "$1" in
+  -h* | --hel*)
+    echo """
+Tipos de mensagens:
+  -w, --warning 
+  -e, --error 
+  -s, --security
+  --------------
+  Mensagens vazias serão enviadas como tipos padrão
+    """
+    ;;
+  -w* | --warn*)
+    _warning "$2"
+    ;;
+  -e* | --err*)
+    _error "$2"
+    ;;
+  -s* | --sec* )
+    _sec "$2"
+    ;;
+  *)
+    _msg "$1"
+    ;;
+esac
