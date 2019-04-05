29bc3978dbb5e0f88c83dd56ed959474eecaa562
root <root@autoforce.com.br>
2019-04-05 18:21:35 +0000

    :fire: bot refatorado para que funcione de forma generalista

--- bot.sh
+++ bot.sh
@@ -1,47 +1,60 @@
 #!/usr/bin/env bash
 
-_send(){
-  curl -X POST -H 'Content-type: application/json' \
-  --data "{\"text\":\"$1\"}" \
-  $YouBot
-}
-
-_warning(){
-  _send ":eyes: *Atenção*: $1"
-}
-
-_error(){
-  _send ":warning: *Erro*: $1"
-}
-
-_sec(){
-  _send ":lock: *Sec.*: $1"
-}
-
-_msg(){
-  _send ":hangloose: *Mensagem*: $1"
-}
-case "$1" in
-  -h* | --hel*)
-    echo """
+help="""
+bot <Chat ID> <option/msg> <msg:optional>
 Tipos de mensagens:
   -w, --warning 
   -e, --error 
   -s, --security
   --------------
   Mensagens vazias serão enviadas como tipos padrão
-    """
+"""
+
+_send(){
+  # 1: Id of chat
+  # 2: Msg content
+  curl -X POST -H 'Content-type: application/json' \
+  --data "{\"text\":\"$2\"}" \
+  $1
+}
+
+_warning(){
+  _send "$1" ":eyes: *Atenção*: $2"
+}
+
+_error(){
+  _send "$1" ":warning: *Erro*: $2"
+}
+
+_sec(){
+  _send "$1" ":lock: *Sec.*: $2"
+}
+
+_msg(){
+  _send "$1" ":hangloose: *Mensagem*: $2"
+}
+
+case "$1" in
+  -h* | --hel*)
+    echo "$help"
     ;;
-  -w* | --warn*)
-    _warning "$2"
-    ;;
-  -e* | --err*)
-    _error "$2"
-    ;;
-  -s* | --sec* )
-    _sec "$2"
-    ;;
-  *)
-    _msg "$1"
+    * )
+	case "$2" in
+	  -h* | --hel*)
+	    echo "$help"
+	    ;;
+	  -w* | --warn*)
+	    _warning "$1" "$3"
+	    ;;
+	  -e* | --err*)
+	    _error "$1" "$3"
+	    ;;
+	  -s* | --sec* )
+	    _sec "$1" "$3"
+	    ;;
+	  *)
+	    _msg "$1" "$2"
+	    ;;
+	esac
     ;;
 esac
