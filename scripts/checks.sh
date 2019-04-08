5b41d8a17af062b2225e66ab7509cae7f2c86d9c
jul10l1r4 <jul10l1r4@disroot.org>
2019-04-08 10:59:56 -0300

    💐, Corrigido problema em envio de mensagens

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
+
+# Default
+ninxOn="$(cat '../nginxOn')"
+
 if /usr/sbin/service nginx status | grep -q "active (running)"; then
     echo -e "$success Nginx ok"
     _clear_rules
-    printf '0' > ../nginxOn
+    if [ "$ninxOn" != "0" ];then
+      printf '0' > ../nginxOn
+      _notify_success
+    fi
 else
-  if [ "$(cat '../nginxOn')" == "0" ];then
+  if [ "$nginxOn" == "0" ];then
     echo -e "$error Nginx inativo"
     _check
   else
     echo -e "$error Nginx continua OFF"
     _recheck
   fi
-
-#    case "$(cat '../nginxOn')" in
-#      "0" )
-#        echo -e "$error Nginx continua off"
-#        
-#      ;;
-#      "1" )
-#        echo -e "$error Nginx inativo"
-#        _check
-#        
-##        _redirect "$slaveIp"
-##        _notify "$slaveIp"
-#      ;;
-#    esac
 fi
  
