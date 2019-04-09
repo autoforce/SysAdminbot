d52a2b4fd3e005b0448b19ad9102b3cd5221eaa1
jul10l1r4 <jul10l1r4@disroot.org>
2019-04-09 10:58:50 -0300

    :tada:, Feature, check if 3MB

--- scripts/details.sh
+++ scripts/details.sh
@@ -10,12 +10,12 @@ _getMemory(){
 
 _memory(){
   percent="$(free | grep Mem | awk '{print $4}')"
-  if [ "$percent" -lt 524288 ];then
-    bot "$verbose" -e "Memória ocupando menos de 500 MB, veja os *processos* que estão *consumindo*:"
+  if [ "$percent" -lt 224288 ];then
+    bot "$verbose" -e "Memória ocupando menos de 200 MB, veja os *processos* que estão *consumindo*:"
     bot "$verbose" -e "\`\`\`$(_getMemoryProcess)\`\`\`"
     bot "$verbose" -e "veja o seu consumo:\`\`\`$(_getMemory)\`\`\`"
-  elif [ "$percent" -lt "624288" ];then
-    bot "$verbose" -w "Memória ocupando 600 MB"
+  elif [ "$percent" -lt "524288" ];then
+    bot "$verbose" -w "Memória ocupando 500 MB"
   fi
 }
 
